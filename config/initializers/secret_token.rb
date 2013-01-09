f5f1e95a97bce4c58c1d79860927d0d0cbc6b4fa
Ahmad Saifuddin <superdeen@brainstorm.pro>
2013-01-09 23:38:41 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StageDummy::Application.config.secret_token = '09e24f67a103b88e169b334ed54c20285560a17a664c342f633f245255a6ac784b49c0bb681ee3c375bb54140fad75d17cce7f95d299415938803e8af8209810'
